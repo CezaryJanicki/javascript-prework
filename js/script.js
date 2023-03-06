11bb6ea0105517e1d993e50587f578f571c2665f
CezaryJanickiMMS <janickic@media-saturn.com>
2023-03-06 18:09:42 +0100

    Add playerMove

--- js/script.js
+++ js/script.js
@@ -7,9 +7,25 @@ let computerMove = 'nieznany ruch';
 if (randomNumber == 1) {
   computerMove = 'kamień';
 } else if (randomNumber == 2) {
-    computerMove = 'nozyce'
-} else {
     computerMove = 'papier'
+} else {
+    computerMove = 'nożyce'
 }
 
 // printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (randomNumber == 2) {
+    computerMove = 'papier'
+} else {
+    computerMove = 'nożyce'
+}
+
+printMessage('Twój ruch to: ' + playerMove);
