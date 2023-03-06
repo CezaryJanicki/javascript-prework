1a1c0bea944be4c6ee7bd065ebbc156d54f88f86
CezaryJanickiMMS <janickic@media-saturn.com>
2023-03-06 18:27:06 +0100

    Game v1.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+    playerMove = 'papier'
+} else if (playerInput == '3') {
+    playerMove = 'nożyce'
+} else {
+    playerMove = 'nieznany ruch';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 // console.log('Wylosowana liczba to: ' + randomNumber);
@@ -12,20 +30,14 @@ if (randomNumber == 1) {
     computerMove = 'nożyce'
 }
 
-// printMessage('Mój ruch to: ' + computerMove);
+printMessage('Ruch komputera to: ' + computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else if (randomNumber == 2) {
-    computerMove = 'papier'
-} else {
-    computerMove = 'nożyce'
-}
-
-printMessage('Twój ruch to: ' + playerMove);
+if (computerMove == 'kamień' && playerMove == 'kamień' || computerMove == 'nożyce' && playerMove == 'nożyce' || computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis!');
+  } else if (computerMove == 'kamień' && playerMove == 'nożyce' || computerMove == 'nożyce' && playerMove == 'papier' || computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Komputer Wygrywa!');
+  } else if (playerMove == 'nieznany ruch') {
+    printMessage('Nieznany ruch. Nikt nie wygrywa.');
+  } else {
+    printMessage('Ty wygrywasz!');
+  }
