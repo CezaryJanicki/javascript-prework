c8eaf92c6fed8e4ca24fbf38130a262a5e2acbf0
CezaryJanickiMMS <janickic@media-saturn.com>
2023-03-06 17:56:39 +0100

    Add computerMove variable

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-printMessage('Zagrałem ' + 'papier' + '! Jeśli Twój ruch to papier, to wygrywasz!');
+let computerMove = `papier`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
